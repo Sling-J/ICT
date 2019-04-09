b578eef5bc96189d90fe1566ecff64ec95cd1be4
Sling-J <nurel32gmail.com>
2019-04-09 18:26:58 +0600

    add (parallax/bg-slider/pagination-design/addForm-design)

--- ICTapp/static/js/custom.js
+++ ICTapp/static/js/custom.js
@@ -1,23 +1,23 @@
-// const content = $('header');
-// const backgrounds = [
-//    "url('../img/1.jpg')",
-//    "url('../img/2.jpg')",
-//    "url('../img/3.jpg')",
-//    "url('../img/4.jpg')",
-//    "url('../img/5.jpg')",
-// ];
+const content = $('header');
+const backgrounds = [
+   "url('/media/img/1.jpg')",
+   "url('/media/img/2.jpg')",
+   "url('/media/img/3.jpg')",
+   "url('/media/img/4.jpg')",
+   "url('/media/img/5.jpg')",
+];
 
-// let current = 0;
+let current = 0;
 
-// function nextBackground() {
-//    current++;
-//    current = current % backgrounds.length;
-//    content.css('background-image', backgrounds[current]);
-// };
+function nextBackground() {
+   current++;
+   current = current % backgrounds.length;
+   content.css('background-image', backgrounds[current]);
+};
 
-// setInterval(nextBackground, 6000);
+setInterval(nextBackground, 6000);
 
-// content.css('background-image', backgrounds[current]);
+content.css('background-image', backgrounds[current]);
 
 const inputArr = document.querySelectorAll('.subscribe input');
 const subscribeTextarea = document.querySelectorAll('.subscribe textarea')[0];
