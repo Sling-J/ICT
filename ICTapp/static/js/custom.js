0f8ae99a3b993f12ceefc373413ec05ac9af5e48
Sling-J <nurel32gmail.com>
2019-04-30 18:51:22 +0600

    fix (compress / cache, onLoad слайдер)

--- ICTapp/static/js/custom.js
+++ ICTapp/static/js/custom.js
@@ -1,23 +1,44 @@
-const content = $('header');
-const backgrounds = [
-   "url('/media/img/1.jpg')",
-   "url('/media/img/2.jpg')",
-   "url('/media/img/3.jpg')",
-   "url('/media/img/4.jpg')",
-   "url('/media/img/5.jpg')",
-];
+// Кэш
+function preloadImages() {
+   const array = [
+      '/media/img/1.jpg',
+      '/media/img/2.jpg',
+      '/media/img/3.jpg',
+      '/media/img/4.jpg',
+      '/media/img/5.jpg'
+   ];
 
-let current = 0;
+   if (!preloadImages.list) preloadImages.list = [];
 
-function nextBackground() {
-   current++;
-   current = current % backgrounds.length;
-   content.css('background-image', backgrounds[current]);
+   const list = preloadImages.list;
+
+   array.forEach(item => {
+      const img = new Image();
+
+      img.addEventListener('load', () => {
+         const idx = list.indexOf(this);
+         if (idx !== -1) list.splice(idx, 1)
+      });
+
+      img.src = item;
+      list.push(img);
+   });
+
+   const backgrounds = list.map(item => `url('${item.src}')`);
+   const content = document.querySelector('header');
+   let current = 0;
+
+   function nextBackground() {
+      current++;
+      current = current % backgrounds.length;
+      content.style.backgroundImage = backgrounds[current];
+   }
+
+   setInterval(nextBackground, 6000);
+   content.style.backgroundImage = backgrounds[current];
 }
 
-setInterval(nextBackground, 6000);
-
-content.css('background-image', backgrounds[current]);
+window.addEventListener('load', preloadImages);
 
 const inputArr = document.querySelectorAll('.subscribe input');
 const subscribeTextarea = document.querySelectorAll('.subscribe textarea')[0];
