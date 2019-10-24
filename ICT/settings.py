df5d261b3b4602497b2423cc409c587b812377b3
Daryn <kapa.miki16@gmail.com>
2019-10-24 19:16:13 +0600

    change static settings

--- ICT/settings.py
+++ ICT/settings.py
@@ -123,13 +123,13 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 
 MEDIA_URL = '/media/'
