aa7a62e925dd4624844878028146e150d66a14fb
Daryn <kapa.miki16@gmail.com>
2019-10-25 21:20:59 +0600

    fix

--- ICTapp/urls.py
+++ ICTapp/urls.py
@@ -11,3 +11,6 @@ urlpatterns = [
    path('staff/', StaffView.as_view(), name='staff_url'),
    path('staff/<int:pk>/', StaffDetailView.as_view(), name='staff_detail_url')
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
