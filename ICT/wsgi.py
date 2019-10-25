147e8a157eb95ee5f3b60f54bf4a634ebd3684d2
Daryn <kapa.miki16@gmail.com>
2019-10-25 21:16:09 +0600

    fix

--- ICT/wsgi.py
+++ ICT/wsgi.py
@@ -4,4 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ICT.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
